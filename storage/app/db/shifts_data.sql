941a78e5ab500001f71df649de9b5b3c1bb30060
Martin Ramirez <mramireza272@gmail.com>
2019-06-03 19:47:05 -0500

    se eliminan archivos innecesarios

--- storage/app/db/shifts_data.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO public.shift_names (id, shift_name, active, created_by, created_at, updated_at) VALUES (1, 'TIEMPO COMPLETO', true, 1, '2019-02-09 09:34:07', '2019-02-09 09:34:07');
-INSERT INTO public.shift_names (id, shift_name, active, created_by, created_at, updated_at) VALUES (2, 'JORNADA AMPLIADA', true, 1, '2019-02-09 09:34:07', '2019-02-09 09:34:07');
-INSERT INTO public.shift_names (id, shift_name, active, created_by, created_at, updated_at) VALUES (3, 'MATUTINO', true, 1, '2019-02-09 09:34:07', '2019-02-09 09:34:07');
-INSERT INTO public.shift_names (id, shift_name, active, created_by, created_at, updated_at) VALUES (4, 'VESPERTINO', true, 1, '2019-02-09 09:34:07', '2019-02-09 09:34:07');
-INSERT INTO public.shift_names (id, shift_name, active, created_by, created_at, updated_at) VALUES (5, 'NOCTURNO', true, 1, '2019-02-09 09:34:07', '2019-02-09 09:34:07');
-INSERT INTO public.shift_names (id, shift_name, active, created_by, created_at, updated_at) VALUES (6, 'DISCONTINUO', true, 1, '2019-02-21 01:26:12', '2019-02-21 01:26:12');
-INSERT INTO public.shift_names (id, shift_name, active, created_by, created_at, updated_at) VALUES (7, 'MIXTO', true, 1, '2019-02-21 01:26:12', '2019-02-21 01:26:12');
-
